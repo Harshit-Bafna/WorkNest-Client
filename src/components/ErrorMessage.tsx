69b2f4ee64952e789280db2961f48b1e64b29aec
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-01-16 22:35:18 +0530

    feat: Error messsage component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface ErrorMessageProps {
+    errorMessage: string
+}
+
+const ErrorMessage: FC<ErrorMessageProps> = ({ errorMessage }) => {
+    return (
+        <div className="bg-off-white absolute top-3 right-3 shadow-input-shadow px-5 py-5 w-64 z-50 border-l-4 border-bright-blue rounded-tr-xl rounded-br-xl select-none">
+            <p className="text-dark-grey text-base font-inter font-medium">{errorMessage}</p>
+        </div>
+    )
+}
+
+export default ErrorMessage
