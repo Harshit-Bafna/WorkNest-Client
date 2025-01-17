92922d0985cde9ab577d86d24cf8d2dc761640f2
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-01-17 11:28:04 +0530

    fix: Styling

--- src/pages/Auth/Components/LeftContainer.tsx
+++ src/pages/Auth/Components/LeftContainer.tsx
@@ -18,7 +18,7 @@ const LeftContainer: FC<LeftContainerProps> = ({ leftContainer }) => {
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center',
             }}
-            className="bg-light-blue md:p-12 p-6 md:max-w-96 max-w-72 rounded-lg flex flex-col shrink md:text-left text-center">
+            className="bg-light-blue md:p-12 p-6 md:max-w-96 max-w-72 rounded-lg flex flex-col shrink md:text-left justify-center text-center">
             <div className="font-semibold font-poppins text-xl">{leftContainer.header}</div>
             <div className="font-normal font-inter md:text-base text-sm mt-4 text-light-dark-grey md:leading-7 sm:tracking-wide">
                 {leftContainer.description}
