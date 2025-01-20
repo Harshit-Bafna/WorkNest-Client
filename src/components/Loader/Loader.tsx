3cceb2037b8c342d5e190c46fec57eebdc928d0c
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-01-20 13:41:32 +0530

    fix: Position of loader when page is scrolled

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,17 +3,15 @@ import './loader.css'
 
 const Loader: FC = () => {
     useEffect(() => {
-        // Disable scrolling
         document.body.classList.add('no-scroll')
 
         return () => {
-            // Re-enable scrolling when the component is unmounted
             document.body.classList.remove('no-scroll')
         }
     }, [])
 
     return (
-        <div className="absolute h-screen w-screen flex justify-center items-center z-[49] top-0 bg-light-blue">
+        <div className="h-screen w-screen flex justify-center items-center z-[49] top-0 bg-light-blue fixed">
             <div className="loadingspinner">
                 <div id="square1"></div>
                 <div id="square2"></div>
