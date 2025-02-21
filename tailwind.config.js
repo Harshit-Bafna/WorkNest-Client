b3b9d9ed3ed59948f207b929fdb8a49e030bacae
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-02-21 16:18:54 +0530

    style: Tailwind config color update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ export default {
       colors: {
         'bright-blue': '#316eff',
         'light-blue': '#eff2f6',
-
         'off-white': '#f0ffff',
-
         'light-gray': '#f5f5f5',
         'dark-grey': '#3a3a3a',
         'light-dark-grey': '#696969',
-        'gray-muted': '#94a3b8'
+        'gray-muted': '#94a3b8',
+        'vibrant-green': '#4CAF50',
+        'orange': '#f97316',
       },
       dropShadow: {
         'text-shadow': '0px 4px 4px rgba(0, 0, 0, 0.25)',
