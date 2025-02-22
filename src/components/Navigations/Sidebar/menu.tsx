ff74e7e0911ffe813fc5b13ed5d6682952380264
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-02-22 13:44:32 +0530

    fix: Notification Route

--- src/components/Navigations/Sidebar/menu.tsx
+++ src/components/Navigations/Sidebar/menu.tsx
@@ -29,7 +29,7 @@ export const menu: MenuItem[] = [
     },
     {
         title: 'Notifications',
-        path: path.Analytics,
+        path: path.Notifications,
         icon: <Bell size={20} />,
     },
 
